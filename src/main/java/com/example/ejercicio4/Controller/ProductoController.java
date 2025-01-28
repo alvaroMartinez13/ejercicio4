5de95e51fd08c68fd700a421109d40983f30a09d
alvaroMartinez13 <alvaro.martinezalcina13@gmail.com>
2025-01-27 22:37:12 -0500

    Innit Commit

--- src/main/java/com/example/ejercicio4/Controller/ProductoController.java
+++ src/main/java/com/example/ejercicio4/Controller/ProductoController.java
@@ -8,40 +8,35 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.example.ejercicio4.Entity.Producto;
 
 @Controller
 public class ProductoController {
-    
+
     private final List<Producto> productos = new ArrayList<>();
 
-    @GetMapping("/formulario")
-    public String mostrarFormulario(Model model){
-        model.addAttribute("producto", new Producto());
-        return "formulario";
-    }
-
     @GetMapping("/")
-    public String redireccionarFormulario(){return "redirect:/formulario";}
+    public String redireccionarFormulario(){return "redirect:formulario";}
+
+    @GetMapping("/formulario")
+    public String mostrarFormulario(Model model) {
+        model.addAttribute("producto", new Producto());
+        return "formulario"; 
+    }
 
     @PostMapping("/save")
     @ResponseBody
-    public Producto guardarProducto(@ModelAttribute Producto producto){
-        productos.add(producto);
-        return producto;
+    public Producto guardarProducto(@ModelAttribute Producto producto) {
+        productos.add(producto); 
+        return producto; 
     }
 
-    @RestController
-    @RequestMapping("/api")
-    public class ProductoApiController {
-        @GetMapping("/productos")
-        public List<Producto> lista_Productos(){
-            return productos;
-        }
+    @GetMapping("/productos")
+    public String mostrarProductos(Model model) {
+        model.addAttribute("productos", productos);
+        return "productos";
     }
 
 }
